b41e1e8a238100886cde71deb635362bb75853c2
Oscar <osfr1701@student.miun.se>
2018-12-06 17:41:08 +0100

    Det mesta klart

--- logistics.sh
+++ logistics.sh
@@ -2,6 +2,7 @@
 
 function print_help()
 {
+    echo ""
     echo "Usage: logistics FILE [-b|-p|-s {i|n|v|l|b|h}]"
     echo "Used for logistics management with FILE as underlying data."
     echo "  -b      generate backup copy of data contents"
@@ -10,13 +11,30 @@ function print_help()
     echo "          weight (v), length (l) width (b), height (h)," 
     echo "          print data contents and exit"
     echo "  --help display this help and exit"
+    echo ""
+}
+
+function get_filename()
+{
+    while true; do
+        echo "Filename:"
+        read filename
+        if [ -f $filename.txt ]
+        then
+            break
+        else
+            echo "Invalid filename"
+        fi
+    done
 }
 
 function print_file()
 {
+    echo ""
     while IFS=';' read -r id name weight length width height; do
         echo "$id $name $weight $length $width $height"
     done < "$filename".txt
+    echo ""
 }
 
 function backup_file()
@@ -24,9 +42,43 @@ function backup_file()
     cat $filename.txt > $filename.backup
 }
 
+function interactive_menu()
+{
+    while true; do
+        
+        echo "What do you want to do?"
+        echo "(p): print file content, (b): backup file, (s): sort file, (h): print help text, (e): exit program" 
+        echo "Enter an option:"
+        read input
+
+        case $input in
+
+            h)
+                print_help;;
+            p)
+                get_filename
+                print_file;;
+            b)
+                get_filename
+                backup_file;;
+            e)
+                exit;;
+            *)
+                echo "Invalid input, try again"
+                ;;
+        
+        esac
+
+    done
+}
+
+if [ -z "$1" ]
+then interactive_menu
+fi
 
 if [ -n "$3" ] 
 then
+    
     echo "För många argument"
     exit
 fi
@@ -35,12 +87,9 @@ filename="$1"   #Spara filnamnet
 shift 1         #Flytta argumenten ett steg så getopts ignorerar filnamnet
 
 while getopts "hpb-:" arg; do
-
-    #echo $arg
-
     case $arg in
-        -)
-            case "${OPTARG}" in
+        -)  #Kolla efter lång flagga
+            case "${OPTARG}" in 
                 help)
                     print_help
                     exit
@@ -64,9 +113,3 @@ while getopts "hpb-:" arg; do
             ;;
     esac
 done
-
-#Interaktivt läge
-#while true: do
-#    echo Hello, who am I talking to?
-
-#done
