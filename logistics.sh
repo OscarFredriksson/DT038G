53b9c5dd5d6ee444c86d915081801c9a3844ea20
Oscar <osfr1701@student.miun.se>
2018-12-06 19:30:04 +0100

    det mesta färdigt

--- logistics.sh
+++ logistics.sh
@@ -14,17 +14,31 @@ function print_help()
     echo ""
 }
 
+function validate_filename()
+{
+    if [ -f $filename ]
+    then
+        return 0
+    else
+        
+        return -1
+    fi
+}
+
 function get_filename()
 {
     while true; do
         echo "Filename:"
         read filename
-        if [ -f $filename.txt ]
+
+        validate_filename
+        if [ $? == 0 ]
         then
             break
         else
-            echo "Invalid filename"
+            echo "Invalid filename, try again:"
         fi
+
     done
 }
 
@@ -33,13 +47,40 @@ function print_file()
     echo ""
     while IFS=';' read -r id name weight length width height; do
         echo "$id $name $weight $length $width $height"
-    done < "$filename".txt
+    done < "$filename"
     echo ""
 }
 
 function backup_file()
 {
-    cat $filename.txt > $filename.backup
+    backupname="${filename%.*}.backup"   #Tar bort filändelsen och lägger till .backup istället
+
+    cat $filename > $backupname
+}
+
+function sort_file()
+{
+    case $1 in
+        i)
+            sort -n -t $';' -o $filename $filename;;
+        n)
+            sort -k2,2 -t $';' -o $filename $filename;; 
+        v)
+            sort -k3,3 -n -t $';' -o $filename $filename;; 
+        l)
+            sort -k4,4 -n -t $';' -o $filename $filename;; 
+        b)
+            sort -k5,5 -n -t $';' -o $filename $filename.;; 
+        h)
+            sort -k6,6 -n -t $';' -o $filename $filename;;
+        *)
+            echo "invalid column to sort on"
+            return -1;;
+    esac
+    
+
+
+    return 0
 }
 
 function interactive_menu()
@@ -52,7 +93,6 @@ function interactive_menu()
         read input
 
         case $input in
-
             h)
                 print_help;;
             p)
@@ -61,6 +101,21 @@ function interactive_menu()
             b)
                 get_filename
                 backup_file;;
+            s)
+                get_filename
+                while true; do
+                    echo "What do you want to sort on?"
+                    echo "(i): ID, (n): name, (v): weight, (l):length, (b): width, (h): height"
+                    echo "Enter an option:"
+                    read column
+
+                    sort_file "$column"
+                    if [ $? == 0 ]  #Kollar returvärdet
+                    then
+                        break
+                    fi
+                done
+                ;;
             e)
                 exit;;
             *)
@@ -76,17 +131,18 @@ if [ -z "$1" ]
 then interactive_menu
 fi
 
-if [ -n "$3" ] 
+filename="$1"   #Spara filnamnet
+
+validate_filename   #Kolla så filnamnet är giltigt
+if [ $? != 0 ]
 then
-    
-    echo "För många argument"
+    echo "Invalid filename"
     exit
 fi
 
-filename="$1"   #Spara filnamnet
 shift 1         #Flytta argumenten ett steg så getopts ignorerar filnamnet
 
-while getopts "hpb-:" arg; do
+while getopts "hpbs:-:" arg; do
     case $arg in
         -)  #Kolla efter lång flagga
             case "${OPTARG}" in 
@@ -107,6 +163,10 @@ while getopts "hpb-:" arg; do
             backup_file
             exit
             ;;
+        s) 
+            sort_file "${OPTARG}"
+            exit
+            ;; 
         *)  
             echo "invalid option"
             exit 
