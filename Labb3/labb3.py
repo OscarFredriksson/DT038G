976407ac908888f924f9308401f9cc57c55e27cc
Oscar <osfr1701@student.miun.se>
2018-12-18 17:17:33 +0100

    Labb3 klar

--- Labb3/labb3.py
+++ Labb3/labb3.py
@@ -9,94 +9,100 @@ from random import randint
 resource = ""
 readers = 0
 writers = 0
-write_lock = Lock()
 resource_lock = Lock()
+read_lock = Lock()
+edit_readers_lock = Lock()
+edit_writers_lock = Lock()
 
 def acquire_write_lock():
+      edit_writers_lock.acquire()
+      
       global writers
       writers += 1
-      if(writers == 1):
-            resource_lock.acquire()
+      if writers == 1:
+         read_lock.acquire()
 
-      #print("locking...")
-      write_lock.acquire()
+      edit_writers_lock.release()
+      resource_lock.acquire()
 
 
 def release_write_lock():
-   #print("Releasing write lock...")
-   write_lock.release()
-   #print("done!")
+   edit_writers_lock.acquire()
+   resource_lock.release()
+   
    global writers
    writers -= 1
-   if(writers == 0):
-      resource_lock.release()
+   if writers == 0:
+      read_lock.release()
+   
+   edit_writers_lock.release()
    
 
 def acquire_read_lock():   
-      resource_lock.acquire()
+      read_lock.acquire()
+      edit_readers_lock.acquire()
       
       global readers
       readers += 1
       if readers == 1:
-            #print("Write_locked...")
-            write_lock.acquire()
-            #print("Freed!")
+         resource_lock.acquire()
 
-      resource_lock.release()
+      edit_readers_lock.release()
+      read_lock.release()
+
 
 def release_read_lock():
+   edit_readers_lock.acquire()
+
    global readers
    readers -= 1
    if readers == 0:
-      write_lock.release()
+      resource_lock.release()
    
+   edit_readers_lock.release()
 
-
+   
 def write_thread1():
-      while(True):
-            #for i in range(3):
-            acquire_write_lock()
-            #time.sleep(1)
+   for i in range(1000):
+      acquire_write_lock()
+      
+      #time.sleep(1)
 
-            print("writing...")
-            now = datetime.datetime.now()
-            global resource
-            resource = str(now.year) + "-" + str(now.month) + "-" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
-            print("finised writing!")
+      print("Writer1 writing...")
+      now = datetime.datetime.now()
+      global resource
+      resource = str(now.year) + "-" + str(now.month) + "-" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
+      print("finised writing!")
 
-            release_write_lock()
+      release_write_lock()
 
       
 
 def write_thread2():
-      while(True):
-            #for i in range(3):
-            acquire_write_lock()
-            #print("writing...")
+   for i in range(1000):
+      acquire_write_lock()
 
-            #time.sleep(1)
+      #time.sleep(1)
 
-            print("writing...")
-            now = datetime.datetime.now()
-            global resource
-            resource = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " " + str(now.year) + "-" + str(now.month) + "-" + str(now.day)
-            print("finised writing!")
+      print("Writer2 writing...")
+      now = datetime.datetime.now()
+      global resource
+      resource = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " " + str(now.year) + "-" + str(now.month) + "-" + str(now.day)
+      print("finised writing!")
 
-            release_write_lock()
+      release_write_lock()
 
 def read_thread():
-      while(True):
-            #for i in range(3):
-            acquire_read_lock()  
+   for i in range(10):
+      acquire_read_lock()  
 
-            #time.sleep(1)    
+      #time.sleep(1)
 
-            global readers
-            #time.sleep(2)
-            global resource
-            print("Reading: ", resource, " alongside ", readers-1, " other readers.")
-                  
-            release_read_lock()
+      global readers
+      global resource
+      print("Reading: ", resource, " alongside ", readers-1, " other readers.")
+               
+      release_read_lock()
 
 
 Writer1 = Thread(target=write_thread1)
@@ -105,14 +111,15 @@ Writer2 = Thread(target=write_thread2)
 Reader1 = Thread(target=read_thread)
 Reader2 = Thread(target=read_thread)
 Reader3 = Thread(target=read_thread)
+Reader4 = Thread(target=read_thread)
 
-#while(True):
-#Writer1.start()
+
+Writer1.start()
 Writer2.start()
-      #time.sleep(2)
 
 Reader1.start()
 Reader2.start()
 Reader3.start()
+Reader4.start()
 
       
